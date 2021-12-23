8deefbc582c5defe79534a8bf17f0d0204718e47
Berker <berker435@gmail.com>
2021-12-23 11:25:11 +0300

    First commit with love

---
+++ app/src/main/java/com/berker/whatsappclone/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.berker.whatsappclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.berker.whatsappclone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
